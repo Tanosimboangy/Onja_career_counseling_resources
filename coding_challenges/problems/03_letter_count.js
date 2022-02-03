cd8e28c104beca1bfbfd999ab1085e368281e75d
Tanosimboangy <Jacquit.aja@onja.org>
2022-02-03 09:01:10 +0300

    accomplishing the task letter count

--- coding_challenges/problems/03_letter_count.js
+++ coding_challenges/problems/03_letter_count.js
@@ -3,7 +3,22 @@
 // spaces.
 
 function letterCount(str) {
-
+  let count = {}
+  let newStr = str.toLowerCase().split("");
+  let val = newStr.filter((it, ind) => {
+      if(it !== " " && newStr.indexOf(it) === ind) {return it}
+  })
+  function helper(it, arr) {
+      let count = 0;
+    newStr.forEach(el => {
+        if(el === it) {count ++}
+    })
+    return count;
+  }
+  val.forEach(item => {
+      return count[item] = helper(item, newStr)
+  })
+return count;
 }
 
 module.exports = letterCount;
